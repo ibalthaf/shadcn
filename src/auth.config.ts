0718e274ec4fb206bf0ddca6a340f2ff6bfbf1fc
ibalthaf <ibalthaf@gmail.com>
2024-03-10 01:43:25 +0530

    jwt encode overide

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -3,9 +3,21 @@ import GoogleProvider from 'next-auth/providers/google';
 import type { NextAuthConfig } from 'next-auth';
 import { LoginSchema } from '@/schemas';
 import * as API from '@/services/API';
+import * as storageService from '@/helper/storage';
+import { storageKeys } from '@/helper/configs';
 
 export default {
 	trustHost: true,
+	// jwt: {
+	// 	async encode({ salt, secret, token, maxAge }) {
+	// 		console.log({ salt, secret, token, maxAge });
+
+	// 		const apiToken: string = (await storageService.loadString(
+	// 			storageKeys.ACCESS_TOKEN
+	// 		)) as string;
+	// 		return apiToken;
+	// 	},
+	// },
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID,
